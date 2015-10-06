6741439b6feb0d5e37e8ad81f6d2e7940c9d6aac
Pete Zerger <pete.zerger@gmail.com>
2015-10-06 10:09:38 -0500

    SQL DSC configuration

---
+++ PrepareSqlServer.ps1/PrepareSqlServer.ps1
@@ -0,0 +1,198 @@
+#
+# Copyright="© Microsoft Corporation. All rights reserved."
+#
+
+configuration PrepareAlwaysOnSqlServer
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SQLServicecreds,
+
+        [Parameter(Mandatory)]
+        [String]$SqlAlwaysOnEndpointName,
+
+        [UInt32]$DatabaseEnginePort = 1433,
+
+        [String]$DomainNetbiosName=(Get-NetBIOSName -DomainName $DomainName),
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xComputerManagement,CDisk,xActiveDirectory,XDisk,xSql, xSQLServer, xSQLps,xNetworking
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($SQLServicecreds.UserName)", $SQLServicecreds.Password)
+
+    WaitForSqlSetup
+
+    Node localhost
+    {
+        xWaitforDisk Disk2
+        {
+             DiskNumber = 2
+             RetryIntervalSec =$RetryIntervalSec
+             RetryCount = $RetryCount
+        }
+
+        cDiskNoRestart DataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+        }
+
+        xWaitforDisk Disk3
+        {
+             DiskNumber = 3
+             RetryIntervalSec =$RetryIntervalSec
+             RetryCount = $RetryCount
+        }
+
+        cDiskNoRestart LogDisk
+        {
+            DiskNumber = 3
+            DriveLetter = "G"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+        }
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+        }
+        xFirewall DatabaseEngineFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = $DatabaseEnginePort -as [String]
+            Ensure = "Present"
+        }
+
+        xFirewall ScsmSdkPort
+        {
+            Direction = "Inbound"
+            Name = "SCSM-SDK-TCP-In"
+            DisplayName = "SCSM SDK Access (TCP-In)"
+            Description = "Inbound rule for SCSM to allow SDK access."
+            DisplayGroup = "System Center"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "5724"
+            Ensure = "Present"
+        }
+
+        xFirewall CiresonPortalInbound
+        {
+            Direction = "Inbound"
+            Name = "Cireson-Portal-TCP-In"
+            DisplayName = "Listening port for Cireson Portal (TCP-In)"
+            Description = "Inbound rule to allow users to access the Cireson Portal."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "80"
+            Ensure = "Present"
+        }
+
+        xSqlLogin AddDomainAdminAccountToSysadminServerRole
+        {
+            Name = $DomainCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+        }
+
+        xADUser CreateSqlServerServiceAccount
+        {
+            DomainAdministratorCredential = $DomainCreds
+            DomainName = $DomainName
+            UserName = $SQLServicecreds.UserName
+            Password = $SQLServicecreds
+            Ensure = "Present"
+            DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
+        }
+
+        xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
+        {
+            Name = $SQLCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xADUser]CreateSqlServerServiceAccount"
+        }
+
+        LocalConfigurationManager 
+        {
+            ActionAfterReboot = 'StopConfiguration'
+        }
+
+    }
+}
+function Get-NetBIOSName
+{ 
+    [OutputType([string])]
+    param(
+        [string]$DomainName
+    )
+
+    if ($DomainName.Contains('.')) {
+        $length=$DomainName.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainName.Substring(0,$length)
+    }
+    else {
+        if ($DomainName.Length -gt 15) {
+            return $DomainName.Substring(0,15)
+        }
+        else {
+            return $DomainName
+        }
+    }
+}
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
